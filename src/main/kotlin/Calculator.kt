e52a6783929fbee3919194dbb1b29a4f339f1871
rugii913 <rugii913@gmail.com>
2023-12-05 12:32:15 +0900

    입력 숫자 타입 Double로 변경, % 연산 삭제
    *과제 3 조건에 따름

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,12 +1,11 @@
 class Calculator {
 
-    fun calculate(number1: Int, number2: Int, operator: String): Int {
+    fun calculate(number1: Double, number2: Double, operator: String): Double {
         return when (operator) {
             "+" -> number1 + number2
             "-" -> number1 - number2
             "*" -> number1 * number2
             "/" -> number1 / number2
-            "%" -> number1 % number2
             else -> {
                 throw RuntimeException("잘못된 연산자 입력")
             }
--- src/main/kotlin/KotlinAssignment.kt
+++ src/main/kotlin/KotlinAssignment.kt
@@ -2,25 +2,25 @@ fun main() {
     println("number1, number2 두 수에 대한 계산을 진행합니다.")
     println("number1 operator number2 형태로 계산합니다. (예: 5 + 3)")
 
-    var number1: Int
-    var number2: Int
+    var number1: Double
+    var number2: Double
     var operator: String
     while (true) {
         print("\nnumber1을 입력해주세요: ")
         while (true) {
             try {
-                number1 = readln().toInt()
+                number1 = readln().toDouble()
                 break
             } catch (e: NumberFormatException) {
                 print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
             }
         }
 
-        print("\n연산자 operator를 입력해주세요(+, -, /, *, % 중 선택): ")
+        print("\n연산자 operator를 입력해주세요(+, -, /, * 중 선택): ")
         while (true) {
             operator = readln()
-            if (operator !in arrayOf("+", "-", "*", "/", "%")) {
-                print("\n잘못된 입력입니다. 연산자(+, -, *, /, % 중 선택)를 입력해주세요: ")
+            if (operator !in arrayOf("+", "-", "*", "/")) {
+                print("\n잘못된 입력입니다. 연산자(+, -, *, / 중 선택)를 입력해주세요: ")
                 continue
             }
             break
@@ -29,7 +29,7 @@ fun main() {
         print("\nnumber2를 입력해주세요: ")
         while (true) {
             try {
-                number2 = readln().toInt()
+                number2 = readln().toDouble()
                 break
             } catch (e: NumberFormatException) {
                 print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
