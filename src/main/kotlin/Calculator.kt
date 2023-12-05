e642d244f9d9680e198582fba7dbfbc3452588d6
rugii913 <rugii913@gmail.com>
2023-12-05 12:24:23 +0900

    과제 Lv2 - 구조 변경
    *책임 나누기
    ㅁCalculator 클래스는 연산, 예외처리만
    ㅁ대화, 입출력 및 기타 예외처리는 KotlinAssignment에서 담당
    
    *연산자 입력 시 메시지 수정
    연산자 op를 입력해주세요(+, -, /, * 중 선택)
    -> 연산자 operator를 입력해주세요(+, -, /, *, % 중 선택)

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,51 +1,15 @@
 class Calculator {
-    fun calculate() {
-        println("number1, number2 두 수에 대한 계산을 진행합니다.")
-        println("number1 operator number2 형태로 계산합니다. (예: 5 + 3)")
 
-        var number1: Int
-        var number2: Int
-        var operator: String
-        while (true) {
-            print("\nnumber1을 입력해주세요: ")
-            while (true) {
-                try {
-                    number1 = readln().toInt()
-                    break
-                } catch (e: NumberFormatException) {
-                    print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
-                }
+    fun calculate(number1: Int, number2: Int, operator: String): Int {
+        return when (operator) {
+            "+" -> number1 + number2
+            "-" -> number1 - number2
+            "*" -> number1 * number2
+            "/" -> number1 / number2
+            "%" -> number1 % number2
+            else -> {
+                throw RuntimeException("잘못된 연산자 입력")
             }
-
-            print("\n연산자 op를 입력해주세요(+, -, /, * 중 선택): ")
-            while (true) {
-                operator = readln()
-                if (operator !in arrayOf("+", "-", "*", "/", "%")) {
-                    print("\n잘못된 입력입니다. 연산자(+, -, *, /, % 중 선택)를 입력해주세요: ")
-                    continue
-                }
-                break
-            }
-
-            print("\nnumber2를 입력해주세요: ")
-            while (true) {
-                try {
-                    number2 = readln().toInt()
-                    break
-                } catch (e: NumberFormatException) {
-                    print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
-                }
-            }
-
-            break
-        }
-
-        when (operator) {
-            "+" -> println("\n결과는 ${number1 + number2}입니다.")
-            "-" -> println("\n결과는 ${number1 - number2}입니다.")
-            "*" -> println("\n결과는 ${number1 * number2}입니다.")
-            "/" -> println("\n결과는 ${number1 / number2}입니다.")
-            "%" -> println("\n결과는 ${number1 % number2}입니다.")
         }
     }
 }
--- src/main/kotlin/KotlinAssignment.kt
+++ src/main/kotlin/KotlinAssignment.kt
@@ -1,3 +1,44 @@
 fun main() {
-    Calculator().calculate();
+    println("number1, number2 두 수에 대한 계산을 진행합니다.")
+    println("number1 operator number2 형태로 계산합니다. (예: 5 + 3)")
+
+    var number1: Int
+    var number2: Int
+    var operator: String
+    while (true) {
+        print("\nnumber1을 입력해주세요: ")
+        while (true) {
+            try {
+                number1 = readln().toInt()
+                break
+            } catch (e: NumberFormatException) {
+                print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
+            }
+        }
+
+        print("\n연산자 operator를 입력해주세요(+, -, /, *, % 중 선택): ")
+        while (true) {
+            operator = readln()
+            if (operator !in arrayOf("+", "-", "*", "/", "%")) {
+                print("\n잘못된 입력입니다. 연산자(+, -, *, /, % 중 선택)를 입력해주세요: ")
+                continue
+            }
+            break
+        }
+
+        print("\nnumber2를 입력해주세요: ")
+        while (true) {
+            try {
+                number2 = readln().toInt()
+                break
+            } catch (e: NumberFormatException) {
+                print("\n잘못된 입력입니다. 숫자를 입력해주세요: ")
+            }
+        }
+
+        break
+    }
+
+    val result = Calculator().calculate(number1, number2, operator)
+    println("\n결과는 ${result}입니다.")
 }
