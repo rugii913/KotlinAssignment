43d07e4eaed5e0a9517ed7de51f45caacbd31f06
rugii913 <rugii913@gmail.com>
2023-12-07 15:01:26 +0900

    reflection 사용 / 변경점 줄이기
    ㅁGuideMessages
     ㅇ전역으로 있던 변수들을 GuideMessages 객체 안으로 넣음
     ㅇmenuInit() 호출 시 menuList가 준비됨
      - 새로운 서비스가 나와서 menu가 하나 늘어나야 한다면,
      - (1) menuInit()에서 Pair<메뉴 메시지, 호출할 가이드 함수>를 menuList.add(~)하도록 하면 됨
      - (2) 메뉴 메시지를 추가
      - 위 과정에서 뭔가 누락되면 manuList.add(Pair)에서 컴파일 오류가 발생하므로 실수를 방지할 수 있다.
    
    *** kotlin.reflect의 함수 참조는 Java 리플렉션의 Method보다 훨씬 깔끔한 듯함
    - 다만 리플렉션에는 여러 단점이 있고, https://www.bsidesoft.com/8146에서 지적하듯, 코드가 복잡해지고 성능에도 안 좋으며 회피할 수 있는 방법도 있으므로 읽어보자.
    
    ㅁMainGuide
     ㅇGuideMessages에서 메뉴를 준비하도록 menuInit 호출
     ㅇmenu가 추가되어도 메시지 문자열을 한땀한땀 수정할 필요가 없어짐
      (원하시는 서비스의 숫자를 입력해주세요. 아래 for)
     ㅇ숫자이지만 잘못된 숫자를 입력했을 경우 오류 메시지 출력 추가
     ㅇexit(): 종료 함수 및 메시지 추가

--- src/main/kotlin/guide/GuideMessages.kt
+++ src/main/kotlin/guide/GuideMessages.kt
@@ -1,11 +1,20 @@
 package guide
 
-const val ALERT_MESSAGE_NOT_A_NUMBER = "\n잘못된 입력입니다. 숫자를 입력해주세요. >>> \n"
+import kotlin.reflect.KCallable
 
-const val MENU_EXIT = "원하는 서비스가 없습니다."
-const val MENU_CALCULATOR = "계산기"
-const val MENU_HOTEL = "호텔"
+object GuideMessages {
+    const val ALERT_MESSAGE_NOT_A_NUMBER = "\n잘못된 입력입니다. 숫자를 입력해주세요."
+    const val ALERT_MESSAGE_WRONG_NUMBER = "\n잘못된 입력입니다. 서비스의 번호를 확인해주세요."
 
-const val SERVICE_NUMBER_MENU_EXIT = 0
-const val SERVICE_NUMBER_MENU_CALCULATOR = 1
-const val SERVICE_NUMBER_MENU_HOTEL = 2
+    const val MENU_EXIT = "원하는 서비스가 없습니다."
+    const val MENU_CALCULATOR = "계산기"
+    const val MENU_HOTEL = "호텔"
+
+    var menuList: MutableList<Pair<String, KCallable<Unit>>> = mutableListOf()
+
+    fun menuInit() {
+        // menuList.add(Pair(메뉴 메시지, 호출할 함수 참조))
+        menuList.add(Pair(MENU_EXIT, MainGuide::exit))
+        menuList.add(Pair(MENU_CALCULATOR, CalculatorGuide::guide))
+    }
+}
