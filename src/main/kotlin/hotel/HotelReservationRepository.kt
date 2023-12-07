bdf1993242d3d3d0072418c9115b6c753069fd81
rugii913 <rugii913@gmail.com>
2023-12-07 23:06:21 +0900

    심화 Lv2-service, repository 교통 정리
    *원래는 service에 showReservationsWhichHasSameRoomNumber 이런 이름을 가진 메서드를 만드려고 했으나,
    **방 번호가 같은 목록을 가져오는 일, 목록에서 중복되는 날짜가 있는지 확인하는 일, 검증 관련 등 로직이 섞일 것으로 보였음
    **이 클래스의 메서드들이 복잡해질 듯하여 역할을 Repository로 분리함
    ==> 결론적으로 reservationList 필드에 접근하는 것을 repository에서만 하도록 만듦(이에 따라 list를 private으로 만들어줌)
    
    - HotelReservationService
      - list에 접근하는 로직을 repository에 넘김
      - list에 접근하고 싶으면 repository의 메서드를 호출
    
    - HotelReservationRepository
      - 원래 list와 id만 갖고 있었던 상태
      - list에 접근하는 로직을 전부 가져옴

--- src/main/kotlin/hotel/HotelReservationRepository.kt
+++ src/main/kotlin/hotel/HotelReservationRepository.kt
@@ -4,5 +4,32 @@ import java.util.concurrent.atomic.AtomicInteger
 
 object HotelReservationRepository {
     var previousId: AtomicInteger = AtomicInteger()
-    val hotelReservationList: MutableList<HotelReservationInput> = mutableListOf()
+    private val reservationList: MutableList<HotelReservationInput> = mutableListOf()
+
+    fun save(newReservationInput: HotelReservationInput) {
+        reservationList.add(newReservationInput)
+    }
+
+    fun findAll() {
+        if (reservationList.isEmpty()) {
+            println("현재 호텔 전체에 예약된 내역이 없습니다.")
+        }
+
+        for (i in reservationList.indices) {
+            val it = reservationList[i]
+            println("${it.id}. 사용자: ${it.customerName}, 방번호: ${it.roomNumber}, 체크인: ${it.dateCheckIn}, 체크아웃: ${it.dateCheckOut}")
+        }
+    }
+
+    fun findAllOrderByDateCheckIn() {
+        val sortedList = reservationList.asSequence().sortedBy { it.dateCheckIn }.toList()
+
+        if (sortedList.isEmpty()) {
+            println("현재 호텔 전체에 예약된 내역이 없습니다.")
+        }
+        for (i in sortedList.indices) {
+            val it = sortedList[i]
+            println("$i. 사용자: ${it.customerName}, 방번호: ${it.roomNumber}, 체크인: ${it.dateCheckIn}, 체크아웃: ${it.dateCheckOut}")
+        }
+    }
 }
--- src/main/kotlin/hotel/HotelReservationService.kt
+++ src/main/kotlin/hotel/HotelReservationService.kt
@@ -5,7 +5,7 @@ object HotelReservationService {
         val newReservation = HotelReservationInput.Builder()
             .customerName().roomNumber().dateCheckIn().dateCheckOut().build()
 
-        HotelReservationRepository.hotelReservationList.add(newReservation)
+        HotelReservationRepository.save(newReservation)
 
         // Kotlin 랜덤 참고 - https://codechacha.com/ko/kotlin-random-number/
         // - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.ranges/random.html
@@ -22,26 +22,16 @@ object HotelReservationService {
 
     fun showReservations() {
         println("\n호텔 예약자 목록입니다.")
-
-        val hotelReservationList = HotelReservationRepository.hotelReservationList
-        if (hotelReservationList.isEmpty()) {
-            println("현재 호텔 전체에 예약된 내역이 없습니다.")
-        }
-        for (it in HotelReservationRepository.hotelReservationList) {
-            println("${it.id}. 사용자: ${it.customerName}, 방번호: ${it.roomNumber}, 체크인: ${it.dateCheckIn}, 체크아웃: ${it.dateCheckOut}")
-        }
+        HotelReservationRepository.findAll()
     }
 
     fun showReservationsSorted() {
         println("\n호텔 예약자 목록입니다. (체크인 날짜 순 정렬)")
-
-        val hotelReservationList = HotelReservationRepository.hotelReservationList.asSequence().sortedBy { it.dateCheckIn }.toList()
-        if (hotelReservationList.isEmpty()) {
-            println("현재 호텔 전체에 예약된 내역이 없습니다.")
-        }
-        for (i in hotelReservationList.indices) {
-            val it = hotelReservationList[i]
-            println("$i. 사용자: ${it.customerName}, 방번호: ${it.roomNumber}, 체크인: ${it.dateCheckIn}, 체크아웃: ${it.dateCheckOut}")
-        }
+        HotelReservationRepository.findAllOrderByDateCheckIn()
     }
+    
+    // 원래는 여기에 showReservationsWhichHasSameRoomNumber 이런 식으로 만드려고 했으나,
+    // 방 번호가 같은 목록을 가져오는 일, 목록에서 중복되는 날짜가 있는지 확인하는 일, 검증 관련 등 로직이 섞일 것으로 보임
+    // 이 클래스의 메서드들이 복잡해질 듯하여 역할을 Repository로 분리함
+    // ==> 결론적으로 reservationList 필드에 접근하는 것을 repository에서만 하도록 만듦(private으로)
 }
