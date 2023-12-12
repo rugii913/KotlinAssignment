5d024d67dfa13e92d69da8b91baa273918eb1e09
rugii913 <rugii913@gmail.com>
2023-12-13 01:08:38 +0900

    (구조 변경)메시지 관리 일원화 시도 - 나머지 Guide에도 적용
    - FrontGuide에 적용된 guide 메서드의 방식을 다른 Guide에도 동일하게 적용
    
    *더 공통으로 뽑아낼 수 있는 부분이 보이지만 일단은 다음 구현으로 넘어가려고 한다.
    *TODO: Messenger 공통으로 뽑아낼 수 있는 부분 추후에 뽑아낼 것

--- week3/src/main/kotlin/kotlinassignment/week3/guide/BurgersGuide.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/guide/BurgersGuide.kt
@@ -3,13 +3,25 @@ package kotlinassignment.week3.guide
 import kotlinassignment.week3.messenger.BurgersMessenger
 import kotlinassignment.week3.messenger.ContinueState
 import kotlinassignment.week3.messenger.InputMessenger
+import kotlinassignment.week3.messenger.Message
 
 class BurgersGuide: Guide {
+    private val burgersMessenger = BurgersMessenger()
 
     override fun guide(continueState: ContinueState) {
-        BurgersMessenger().write()
+        burgersMessenger.writeMenu()
         val selectedNumber = InputMessenger().readInt()
 
-        if (selectedNumber == 0) continueState.nextGuide = continueState.previousGuide
+        when (selectedNumber) {
+            0 -> {
+                continueState.nextGuide = continueState.previousGuide
+            }
+            null -> {
+                burgersMessenger.write(Message.NOT_INT_INPUT)
+            }
+            else -> {
+                burgersMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
+            }
+        }
     }
 }
--- week3/src/main/kotlin/kotlinassignment/week3/messenger/FrozenCustardMessenger.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/messenger/FrozenCustardMessenger.kt
@@ -1,12 +1,14 @@
 package kotlinassignment.week3.messenger
 
-import kotlinassignment.filter.IntInputFilter
-
 class FrozenCustardMessenger {
 
-    fun write() {
+    fun writeMenu() {
         println("\n[FrozenCustard MENU]")
         println("1. 준비 중입니다. - 매장에서 만든 아이스크림")
         println("0. 뒤로가기\t\t\t| 뒤로가기")
+    }
+
+    fun write(message: Message) {
+        println(message.messegeValue)
     }
 }
