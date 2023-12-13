9ccb9070846a667ae8b439eb237a745769b98203
rugii913 <rugii913@gmail.com>
2023-12-13 18:18:00 +0900

    구조 변경: MenuGroupGuide 사용자 입력 처리 부분 - enum index 활용
    - enum index를 활용하여 nextMenuGroup을 지정해줌
      - MenuGroup이 많이 추가되더라도 메뉴를 추가하는 데에 들이는 수고가 줄어듦
    *** Java에서는 enum index 어떻게 사용하는지 알아보기

--- week3/src/main/kotlin/kotlinassignment/week3/guide/MenuGroupGuide.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/guide/MenuGroupGuide.kt
@@ -17,31 +17,19 @@ class MenuGroupGuide: Guide {
         
         // 사용자의 입력 처리에 대한 부분
         val selectedNumber = InputMessenger().readInt()
-        when (selectedNumber) {
-            1 -> {
-                continueState.nextGuide = MenuItemGuide()
-                continueState.nextMenuGroup = MenuGroup.Burgers
-            }
-            2 -> {
-                continueState.nextGuide = MenuItemGuide()
-                continueState.nextMenuGroup = MenuGroup.FrozenCustard
-            }
-            3 -> {
-                continueState.nextGuide = MenuItemGuide()
-                continueState.nextMenuGroup = MenuGroup.Drinks
-            }
-            0 -> {
-                menuGroupMessenger.write(Message.EXIT)
-                continueState.nextGuide = null
-            }
-            null -> {
-                menuGroupMessenger.write(Message.NOT_INT_INPUT)
-                // 다시 menuGroupGuide의 guide를 호출해야하므로 nextGuide를 set하지 않는다.
-            }
-            else -> {
-                menuGroupMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
-                // 다시 menuGroupGuide의 guide를 호출해야하므로 nextGuide를 set하지 않는다.
-            }
+
+        if (selectedNumber == null) {
+            menuGroupMessenger.write(Message.NOT_INT_INPUT)
+            // 다시 menuGroupGuide의 guide를 호출해야하므로 nextGuide를 set하지 않는다.
+        } else if (selectedNumber == 0) {
+            menuGroupMessenger.write(Message.EXIT)
+            continueState.nextGuide = null
+        } else if (selectedNumber <= menuGroupEntries.size) {
+            continueState.nextGuide = MenuItemGuide()
+            continueState.nextMenuGroup = menuGroupEntries[selectedNumber - 1]
+        } else {
+            menuGroupMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
+            // 다시 menuGroupGuide의 guide를 호출해야하므로 nextGuide를 set하지 않는다.
         }
     }
 }
