a90edfc6d1d1540b2c2cf69fe45d24c86d89b68e
rugii913 <rugii913@gmail.com>
2023-12-14 00:19:55 +0900

    수정(분기 제거): enum class name 활용하여 출력 관련 분기 제거(TODO 하나 처리)
    - 분기하지 않고 파라미터로 받은 데이터를 활용한다. 훨씬 보기 좋아졌다.

--- week3/src/main/kotlin/kotlinassignment/week3/messenger/MenuItemMessenger.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/messenger/MenuItemMessenger.kt
@@ -1,20 +1,12 @@
 package kotlinassignment.week3.messenger
 
 import kotlinassignment.week3.menu.MenuGroup
-import kotlinassignment.week3.menu.MenuGroup.*
 import kotlinassignment.week3.menu.menuItem.MenuItem
 
 class MenuItemMessenger {
 
     fun writeMenuItems(menuList: List<MenuItem>, menuGroup: MenuGroup) {
-
-        when (menuGroup) {
-            // TODO (매끄럽지 않은 처리, 여기서 구현체를 알 필요가 있을까? (commit 9d7fdc) →) enum을 사용해서 덜 번거로워 보이게 되긴 했다.
-            Burgers -> write(Message.MENU_GROUP_BURGERS_INTRODUCE)
-            FrozenCustard -> write(Message.MENU_GROUP_FROZEN_CUSTARD_INTRODUCE)
-            Drinks -> write(Message.MENU_GROUP_DRINKS_INTRODUCE)
-            Beer -> println(Beer.name) // TODO
-        }
+        println("\n[${menuGroup.name} MENU]")
 
         for (index in menuList.indices) {
             println("${index + 1}. ${String.format("%-28s", menuList[index].name)} | W ${menuList[index].price} | ${menuList[index].information}")
