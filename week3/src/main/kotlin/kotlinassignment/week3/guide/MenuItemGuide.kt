3d30f69a9e2e7cf62746600a411ae33c6071338c
rugii913 <rugii913@gmail.com>
2023-12-13 19:47:20 +0900

    수정(기능 추가): repository에서 가져온 list의 index를 이용하여 입력값과 매칭, 선택한 메뉴아이템을 보여줌
    *전체적인 구성은 e6d373에서 MenuGroup에 했던 작업과 유사함
    - MenuItemMessenger에 맡긴 메뉴 선택지 출력 작업 이후 사용자가 숫자를 입력
      -> 입력받을 숫자를 (인덱스 + 1)로 생각할 수 있다.  (menuItemMessenger에서 사용자에게 출력해준 선택지 참고)
      -> list에서 인덱스가 (selectedNumber - 1)인 menuItem을 얻어옴
    
    - 사용자에게 보여준 list나 현재 menuItem을 가져올 list나 같은 list이기 때문에 index에 +-1 해준 부분만 유의하면
      - 사용자가 생각한 menuItem과 Guide가 list에서 index를 통해 갖고 오는 menuItem은 같을 것이다.
    
    - 위 내용을 바탕으로
      - 선택된 menuItem의 이름과 가격을 출력한다.

--- week3/src/main/kotlin/kotlinassignment/week3/guide/MenuItemGuide.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/guide/MenuItemGuide.kt
@@ -18,16 +18,19 @@ class MenuItemGuide: Guide {
 
         // 사용자의 입력 처리에 대한 부분
         val selectedNumber = InputMessenger().readInt()
-        when (selectedNumber) {
-            0 -> {
-                continueState.nextGuide = continueState.previousGuide
-            }
-            null -> {
-                menuItemMessenger.write(Message.NOT_INT_INPUT)
-            }
-            else -> {
-                menuItemMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
-            }
+
+        if (selectedNumber == null) {
+            menuItemMessenger.write(Message.NOT_INT_INPUT)
+            // 다시 같은 메뉴로 돌아오기 위해서 nextGuide를 set하지 않는다.
+        } else if (selectedNumber == 0) {
+            continueState.nextGuide = continueState.previousGuide
+        } else if (selectedNumber <= menuItemList.size) {
+            println("이번에 선택한 메뉴의 이름은 ${menuItemList[selectedNumber - 1].name}!")
+            println("이번에 선택한 메뉴의 가격은 ${menuItemList[selectedNumber - 1].price}원!")
+            println("끝내려면 0을 입력하기!")
+        } else {
+            menuItemMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
+            // 다시 같은 메뉴로 돌아오기 위해서 nextGuide를 set하지 않는다.
         }
     }
 }
