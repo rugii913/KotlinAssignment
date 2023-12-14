a4e857642417f6d08b649c48bc798c232ef80bff
rugii913 <rugii913@gmail.com>
2023-12-14 12:40:41 +0900

    수정 및 추가: 장바구니 추가 기능을  CartGuide에서 담당 / while 제
    - 장바구니 추가 기능을 CartGuide에서 담당
      - 기능 유지한 채로 MenuItemGuide의 addToCart( ) 메서드에서 CartGuide로 역할 넘김
      - while 없앰
        - FlowState의 nextGuide 등을 활용
        - 그러나 점점 이해하기 복잡해지고 있어서 교통정리가 필요할 듯함

--- week3/src/main/kotlin/kotlinassignment/week3/flowState/FlowState.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/flowState/FlowState.kt
@@ -1,9 +1,12 @@
 package kotlinassignment.week3.flowState
 
 import kotlinassignment.week3.cart.Cart
+import kotlinassignment.week3.guide.CartGuide
 import kotlinassignment.week3.guide.Guide
 import kotlinassignment.week3.guide.MenuGroupGuide
+import kotlinassignment.week3.guide.MenuItemGuide
 import kotlinassignment.week3.menu.MenuGroup
+import kotlinassignment.week3.menu.menuItem.MenuItem
 import kotlinassignment.week3.messenger.InputMessenger
 import kotlinassignment.week3.messenger.OutputMessenger
 
@@ -11,10 +14,14 @@ class FlowState {
 
     val outputMessenger = OutputMessenger()
     val inputMessenger = InputMessenger(outputMessenger)
-    val menuGroupGuide = MenuGroupGuide()
+    val menuGroupGuide: Guide = MenuGroupGuide()
+    val menuItemGuide: Guide = MenuItemGuide()
+    val cartGuide: Guide = CartGuide()
     val cart = Cart()
 
     var nextGuide: Guide? = menuGroupGuide
     var previousGuide: Guide = menuGroupGuide
     lateinit var nextMenuGroup: MenuGroup
+
+    lateinit var targetMenuItem: MenuItem
 }
--- week3/src/main/kotlin/kotlinassignment/week3/guide/MenuItemGuide.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/guide/MenuItemGuide.kt
@@ -1,7 +1,6 @@
 package kotlinassignment.week3.guide
 
 import kotlinassignment.week3.flowState.FlowState
-import kotlinassignment.week3.menu.menuItem.MenuItem
 import kotlinassignment.week3.menu.menuItem.MenuItemRepository
 import kotlinassignment.week3.messenger.InputMessenger
 import kotlinassignment.week3.messenger.Message
@@ -23,31 +22,11 @@ class MenuItemGuide: Guide {
         when (selectedNumber) {
             0 -> flowState.nextGuide = flowState.previousGuide
             in 1..menuItemList.size -> {
-                addToCart(menuItemList[selectedNumber - 1], flowState)
-                flowState.nextGuide = flowState.menuGroupGuide // 메인 메뉴로
+                flowState.nextGuide = flowState.cartGuide
+                flowState.previousGuide = flowState.menuItemGuide
+                flowState.targetMenuItem = menuItemList[selectedNumber - 1]
             }
             else -> flowState.outputMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER) // 다시 같은 메뉴로 돌아오기 위해서 nextGuide를 set하지 않는다.
         }
-    }
-
-    private fun addToCart(menuItem: MenuItem, flowState: FlowState) {
-        var pair: Pair<InputMessenger.InputStatus, Int>
-
-        while (true) { // TODO 여기에서도 while을 없앨 수 있는 방법 고민
-            println("\n${menuItem.name} | W ${menuItem.price} | ${menuItem.information}") // TODO 메시지 출력은 OutputMessenger로
-            println("\n위 메뉴를 장바구니에 추가하시겠습니까?")
-            println("\n1. 확인\t\t2. 취소")
-            pair = flowState.inputMessenger.readInt()
-
-            if (pair.second !in 1..2) { // TODO 입력값이 1, 2가 아닐 때 더 깔끔하게 처리할 수 있는 방법?
-                flowState.outputMessenger.write(Message.NO_CORRESPONDING_SERVICE_NUMBER)
-                continue
-            }
-
-            if (pair.first == InputMessenger.InputStatus.NORMAL) break
-        }
-
-        if (pair.second == 1) flowState.cart.add(menuItem)
-            .run { println("\n${menuItem.name}가 장바구니에 추가되었습니다.") }
     }
 }
