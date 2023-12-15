195711256957ffe87b50b5b8b5ef6bdae876a259
rugii913 <rugii913@gmail.com>
2023-12-15 12:04:47 +0900

    수정: readInt(Int) 예외 처리 중복 부분 제거(내부 호출)

--- week3/src/main/kotlin/kotlinassignment/week3/messenger/InputMessenger.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/messenger/InputMessenger.kt
@@ -6,7 +6,7 @@ class InputMessenger(private val outputMessenger: OutputMessenger) {
 
     private val intInputFilter = IntInputFilter()
 
-    fun readInt(): Pair<InputStatus, Int> = print(">>> ").run {
+    private fun readInt(): Pair<InputStatus, Int> = print(">>> ").run {
         val selectedNumber: Int
 
         try {
@@ -19,22 +19,15 @@ class InputMessenger(private val outputMessenger: OutputMessenger) {
         return Pair(InputStatus.SUCCESS, selectedNumber)
     }
 
-    fun readInt(endInt: Int): Pair<InputStatus, Int> = print(">>> ").run {
-        val selectedNumber: Int
-
-        try {
-            selectedNumber = intInputFilter.filterToInt(readln())
-        } catch (e: NumberFormatException) {
-            outputMessenger.writeExceptionWarningIntRequired()
-            return Pair(InputStatus.FAIL_INT_REQUIRED, Int.MIN_VALUE)
-        }
+    fun readInt(endInt: Int): Pair<InputStatus, Int> {
+        var (inputStatus, selectedNumber) = readInt()
 
         if (selectedNumber > endInt || selectedNumber < 0) {
             outputMessenger.writeWarningIntOutOfRange()
-            return Pair(InputStatus.FAIL_INT_OUT_OF_RANGE, selectedNumber)
-        } else {
-            return Pair(InputStatus.SUCCESS, selectedNumber)
+            inputStatus = InputStatus.FAIL_INT_OUT_OF_RANGE
         }
+
+        return Pair(inputStatus, selectedNumber)
     }
 
     enum class InputStatus {
