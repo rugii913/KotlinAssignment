5fb017fdc262046e4b2ae46eb8c53372ceebb18d
rugii913 <rugii913@gmail.com>
2023-12-15 12:31:48 +0900

    수정: OrderGuide 쓸모 없는 분기 제거
    - OrderGuide 쓸모 없는 분기 제거
      - 입력이 2인 경우 미리 앞에서 쫓아내서 쓸 데 없이 when이 한 번 더 들어갔던 부분을 제거했다.

--- week3/src/main/kotlin/kotlinassignment/week3/guide/OrderGuide.kt
+++ week3/src/main/kotlin/kotlinassignment/week3/guide/OrderGuide.kt
@@ -20,40 +20,40 @@ class OrderGuide(val orderRepository: OrderRepository): Guide {
 
         // 사용자의 입력 처리에 대한 부분
         val (inputStatus, selectedNumber) = flowState.inputMessenger.readInt(2)
-        if (inputStatus != InputMessenger.InputStatus.SUCCESS) return
 
-        when (selectedNumber) {
-            1 -> {
-                if (cartItemList.size == 0) { // TODO 여기서 분기 처리 하지 말고 더 나은 방법이 있을 것 같다.
-                    println("\n장바구니에 아무 것도 없는 경우 주문할 수 없어요.")
-                    flowState.nextGuide = flowState.menuGroupGuide
-                    return
-                }
+        if (inputStatus != InputMessenger.InputStatus.SUCCESS) {
+            return // 입력이 잘못된 경우 다시 이 OrderGuide로 들어온다.
+        } else if (selectedNumber == 2) {
+            flowState.nextGuide = flowState.menuGroupGuide
+            return // 입력이 2인 경우 menuGroupGuide로 보낸다.
+        }
 
-                val (paymentStatus, balance) = SomeExternalInterfaceRepresentingPayments.pay(totalPrice)
 
-                when (paymentStatus) {
-                    SomeExternalInterfaceRepresentingPayments.PaymentStatus.SUCCESS -> {
-                        orderRepository.save(Order(cartItemList))
-                        flowState.cart.clear()
-                        println("\n결제를 완료했어요. (${LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"))})")
-                    }
-                    SomeExternalInterfaceRepresentingPayments.PaymentStatus.EXCEPTION_INSUFFICIENT_FUNDS -> {
-                        println("\n현재 잔액은 ${balance}원으로 ${totalPrice - balance}원이 부족해서 주문할 수 없어요.")
-                    }
-                    SomeExternalInterfaceRepresentingPayments.PaymentStatus.EXCEPTION_NOT_AVAILABLE_TIME -> {
-                        val inspectionStartTime =  SomeExternalInterfaceRepresentingPayments.inspectionStartTime
-                        val inspectionEndTime = SomeExternalInterfaceRepresentingPayments.inspectionEndTime
+        if (cartItemList.size == 0) { // TODO 여기서 분기 처리 하지 말고 더 나은 방법이 있을 것 같다.
+            println("\n장바구니에 아무 것도 없는 경우 주문할 수 없어요.")
+            flowState.nextGuide = flowState.menuGroupGuide
+            return
+        }
 
-                        println("\n현재 시각은 ${LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분"))}이에요.")
-                        println("\n은행 점검 시간($inspectionStartTime ~ ${inspectionEndTime}) 중이라 결제할 수 없어요.")
-                    }
-                }
-                flowState.nextGuide = flowState.menuGroupGuide
+        val (paymentStatus, balance) = SomeExternalInterfaceRepresentingPayments.pay(totalPrice)
+
+        when (paymentStatus) {
+            SomeExternalInterfaceRepresentingPayments.PaymentStatus.SUCCESS -> {
+                orderRepository.save(Order(cartItemList))
+                flowState.cart.clear()
+                println("\n결제를 완료했어요. (${LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"))})")
             }
-            2 -> {
-                flowState.nextGuide = flowState.menuGroupGuide
+            SomeExternalInterfaceRepresentingPayments.PaymentStatus.EXCEPTION_INSUFFICIENT_FUNDS -> {
+                println("\n현재 잔액은 ${balance}원으로 ${totalPrice - balance}원이 부족해서 주문할 수 없어요.")
+            }
+            SomeExternalInterfaceRepresentingPayments.PaymentStatus.EXCEPTION_NOT_AVAILABLE_TIME -> {
+                val inspectionStartTime =  SomeExternalInterfaceRepresentingPayments.inspectionStartTime
+                val inspectionEndTime = SomeExternalInterfaceRepresentingPayments.inspectionEndTime
+
+                println("\n현재 시각은 ${LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분"))}이에요.")
+                println("\n은행 점검 시간($inspectionStartTime ~ ${inspectionEndTime}) 중이라 결제할 수 없어요.")
             }
         }
+        flowState.nextGuide = flowState.menuGroupGuide
     }
 }
