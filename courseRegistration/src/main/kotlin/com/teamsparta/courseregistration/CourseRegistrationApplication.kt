5bb2b04edae44c93a17d7d036114ec3c1c3fa019
rugii913 <rugii913@gmail.com>
2023-12-18 17:11:02 +0900

    main 파일에 함께 있는 class에 open 붙임 - open이 붙어있지 않은 경우 실행 시 오류 발생
    - 앞과 같이 모듈들 추가 진행한 경우, 앱 메인 함수의 class에 open이 붙어있지 않으면 실행 시 오류 발생
      - 에러 메시지: org.springframework.beans.factory.parsing.BeanDefinitionParsingException: Configuration problem: @Configuration class 'Week4Application' may not be final. Remove the final modifier to continue.
      - 왜 이렇게 되는지 찾아볼 것...
    - 아직 관련 문제가 충분히 해결되지 않았다고 판단하여 branch는 남겨둠

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/CourseRegistrationApplication.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/CourseRegistrationApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CourseRegistrationApplication
+open class CourseRegistrationApplication
 
 fun main(args: Array<String>) {
     runApplication<CourseRegistrationApplication>(*args)
--- week4/src/main/kotlin/kotlinassignment/week4/Week4Application.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/Week4Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Week4Application
+open class Week4Application
 
 fun main(args: Array<String>) {
     runApplication<Week4Application>(*args)
