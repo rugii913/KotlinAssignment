6d328588e4620676af46b67682426e8fed3c6567
rugii913 <rugii913@gmail.com>
2023-12-19 16:29:34 +0900

    (예제)CourseService에서 예외 처리할 내용 주석 + 사용할 예외 클래스 추가, 트랜잭션 설정

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/service/CourseServiceImpl.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/service/CourseServiceImpl.kt
@@ -4,6 +4,7 @@ import com.teamsparta.courseregistration.domain.course.dto.CourseResponse
 import com.teamsparta.courseregistration.domain.course.dto.CreateCourseRequest
 import com.teamsparta.courseregistration.domain.course.dto.UpdateCourseRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CourseServiceImpl : CourseService {
@@ -14,21 +15,27 @@ class CourseServiceImpl : CourseService {
     }
 
     override fun getCourseById(courseId: Long): CourseResponse {
+        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
         // TODO: DB에서 ID 기반으로 Course를 가져와서 CourseResponse로 변환 후 반한
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun createCourse(request: CreateCourseRequest): CourseResponse {
         // TODO: request를 Course로 변환 후 DB에 저장
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun updateCourse(courseId: Long, request: UpdateCourseRequest): CourseResponse {
+        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
         // TODO: DB에서 courseId에 해당하는 Course를 가져와서 request 기반으로 업데이트 후 DB에 저장, 저장한 결과를 CourseResponse로 변환 후 반환 
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun deleteCourse(courseId: Long) {
+        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
         // TODO: DB에서 courseId에 해당하는 Course를 삭제, 정책 상 CourseApplication, Lecture는 연관된 Course의 life cycle을 함께하므로 모두 삭제
         TODO("Not yet implemented")
     }
