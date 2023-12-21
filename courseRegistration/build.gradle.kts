5f07e84e8dc8fb295447ba8e0271f26c8498592d
rugii913 <rugii913@gmail.com>
2023-12-21 09:39:16 +0900

    (예제)entity 작성 전 Kotlin에서 JPA entity 사용하기 위해 필요한 플러그인 설정 및 적용

--- courseRegistration/build.gradle.kts
+++ courseRegistration/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.noarg") version "1.8.22"
 }
 
 group = "com.teamsparta"
@@ -12,6 +13,18 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 configurations {
