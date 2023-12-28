5971d495bc04cfd213bc2c903daa385e62721a75
rugii913 <rugii913@gmail.com>
2023-12-28 16:03:47 +0900

    local에서 테스트하기 편하도록 스크립트를 사용하여 DB table schema 및 data 초기화
    - local에서 테스트하기 편하도록 스크립트를 사용하여 DB table schema 및 data 초기화
      - cf. hibernate에 의한 초기화와 스크립트 기반 초기화를 동시에 사용하는 것은 권장되지 않는다.

---
+++ week4/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into to_do_card(id, title, description, user_name, created_date_time)
+values (1, '제목1', '할 일 상세1', '유저1', CURRENT_TIMESTAMP),
+       (2, '두번째 글 제목', '상세한 할 일 2', '이름2', CURRENT_TIMESTAMP);
