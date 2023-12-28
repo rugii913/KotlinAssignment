fb5cf83eaa3a3aa05fb20cb043d58dbe81989591
rugii913 <rugii913@gmail.com>
2023-12-28 20:33:10 +0900

    bean validation 추가, 일관성 있는 exception handling을 위한 null 가능 타입 사용
    - 일관성 있는 exception handling을 위한 null 가능 타입 사용
      - request dto 필드 및 path variable 파라미터 타입에서 null 가능 타입 사용
      - TODO: null 가능 타입 때문에 !!를 사용하는데 괜찮을까?
    
    - request dto들에서 bean validation 적용
    - controller의 path variable들에서 bean validation 적용
    
    - GlobalExceptionHandler에서 처리하는 예외 3건 추가
      - BindException: dto로 매핑 실패할 경우
      - MethodArgumentTypeMismatchException: path variable에 잘못된 타입 들어올 경우
      - ConstraintViolationException: path variable이 null로 들어올 경우
        - ConstraintViolationException의 경우 일관성 있게 다루기 힘들다.
        - 억지로 path variable로 null이 들어올 수 있도록 API를 뚫어준 뒤 예외 처리하는 형태가 됐다.
        - TODO: 이 부분 더 좋은 방법이 없는지?

--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardCreateRequest.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardCreateRequest.kt
@@ -1,10 +1,12 @@
 package kotlinassignment.week4.domain.toDoCard.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
 
 data class ToDoCardCreateRequest(
-    val title: String,
+    @field: NotBlank(message = "제목은 필수값입니다.") val title: String?,
     val description: String?,
-    val userName: String,
-    val createdDateTime: LocalDateTime,
+    @field: NotBlank(message = "작성자 이름은 필수값입니다.") val userName: String?,
+    @field: NotNull(message = "작성일은 필수값입니다.") val createdDateTime: LocalDateTime?,
 )
