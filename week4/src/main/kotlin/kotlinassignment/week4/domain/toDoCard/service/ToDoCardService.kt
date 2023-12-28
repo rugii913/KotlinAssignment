131d9e3631cf5eb34b2e84f04afad32c57013851
rugii913 <rugii913@gmail.com>
2023-12-28 23:12:59 +0900

    service에 transactional 추가, updateToDoCard() 메서드 일부 수정
    - service layer에 transactional 추가,
    - updateToDoCard() 메서드에서 구조 분해 사용하도록 수정

--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/service/ToDoCardService.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/service/ToDoCardService.kt
@@ -9,7 +9,9 @@ import kotlinassignment.week4.domain.toDoCard.model.toResponse
 import kotlinassignment.week4.domain.toDoCard.repository.ToDoCardRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional(readOnly = true)
 @Service
 class ToDoCardService(
     private val toDoCardRepository: ToDoCardRepository,
@@ -27,6 +29,7 @@ class ToDoCardService(
         return toDoCard.toResponse()
     }
 
+    @Transactional
     fun createToDoCard(request: ToDoCardCreateRequest): ToDoCardResponse {
         val toDoCard = ToDoCard(
             title = request.title!!,
@@ -38,16 +41,20 @@ class ToDoCardService(
         return toDoCardRepository.save(toDoCard).toResponse()
     }
 
+    @Transactional
     fun updateToDoCard(toDoCardId: Long?, request: ToDoCardUpdateRequest): ToDoCardResponse {
         val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw ModelNotFoundException("ToDoCard", toDoCardId!!)
 
-        // TODO: request에 속성 추가, 정책상 변경 가능한 속성 변경 등 발생했을 때, 여기에서도 알 수 있도록 컴파일 에러를 내는 식으로 작성하려면 어떻게 해야 하는지 고민
-        toDoCard.title = request.title!!
-        toDoCard.description = request.description
+        // (해결?) request에 속성 추가, 정책상 변경 가능한 속성 변경 등 발생했을 때, 여기에서도 알 수 있도록 컴파일 에러를 내는 식으로 작성하려면 어떻게 해야 하는지?
+        // -> 이런 이유 때문에 굳이 구조 분해 선언 사용했던 듯하다.
+        val (title: String?, description: String?) = request
+        toDoCard.title = title!!
+        toDoCard.description = description!!
 
         return toDoCardRepository.save(toDoCard).toResponse()
     }
 
+    @Transactional
     fun deleteToDoCard(toDoCardId: Long?): Unit {
         // 이 부분이 있어도 select query는 한 번만 나간다. 없는 id로 delete 시도하는 경우 확실한 에러 메시지를 주기 위함
         val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw ModelNotFoundException("ToDoCard", toDoCardId!!)
