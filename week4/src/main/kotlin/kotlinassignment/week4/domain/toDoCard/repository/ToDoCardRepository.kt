0d63fabca4d4af760a318e327cf5e400af89d637
rugii913 <rugii913@gmail.com>
2023-12-28 13:46:00 +0900

    ToDoCardRepository(JPA 사용) 작성

---
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/repository/ToDoCardRepository.kt
@@ -0,0 +1,7 @@
+package kotlinassignment.week4.domain.toDoCard.repository
+
+import kotlinassignment.week4.domain.toDoCard.model.ToDoCard
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ToDoCardRepository : JpaRepository<ToDoCard, Long> {
+}
