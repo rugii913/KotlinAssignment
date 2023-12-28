fc6a7c995e41a76381ed7f3a41db07486c7ff79a
rugii913 <rugii913@gmail.com>
2023-12-28 17:54:05 +0900

    존재하지 않는 id를 가진 모델 요구 시, Spring의 예외 처리 기능 활용
    - 존재하지 않는 id를 가진 모델 요구 시
      - DB 통해서 요구된 id를 가진 record를 탐색 시도했으나 없는 경우
      - controller에서 ModelNotFoundException이 발생하게 함
        - delete 경우, 로직 살짝 수정: 해당 코드를 사용하더라도 select는 한 번만 나감
        - cf. transaction을 사용하지 않더라도 이런 부분 때문에 service layer가 필요할 듯하다.
    - Spring의 예외 처리 기능 이용
      - @ RestControllerAdvice, @ ExceptionHandler 어노테이션 사용
      - ModelNotFoundException이 발생한 경우 잡아내어
      - ErrorResponse를 감싼 ResponseEntity를 반환한다.

--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/controller/ToDoCardController.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/controller/ToDoCardController.kt
@@ -1,5 +1,6 @@
 package kotlinassignment.week4.domain.toDoCard.controller
 
+import kotlinassignment.week4.domain.exception.ModelNotFoundException
 import kotlinassignment.week4.domain.toDoCard.dto.ToDoCardCreateRequest
 import kotlinassignment.week4.domain.toDoCard.dto.ToDoCardResponse
 import kotlinassignment.week4.domain.toDoCard.dto.ToDoCardUpdateRequest
@@ -24,7 +25,7 @@ class ToDoCardController(
 
     @GetMapping("/{toDoCardId}")
     fun getToDoCard(@PathVariable toDoCardId: Long): ResponseEntity<ToDoCardResponse> {
-        val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw IllegalArgumentException("TODO")
+        val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw ModelNotFoundException("ToDoCard", toDoCardId)
 
         return ResponseEntity
             .status(200)
@@ -51,7 +52,7 @@ class ToDoCardController(
         @PathVariable toDoCardId: Long,
         @RequestBody todoCardUpdateRequest: ToDoCardUpdateRequest
     ): ResponseEntity<ToDoCardResponse> {
-        val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw IllegalArgumentException("TODO")
+        val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw ModelNotFoundException("ToDoCard", toDoCardId)
 
         // TODO: request에 속성 추가, 정책상 변경 가능한 속성 변경 등 발생했을 때, 여기에서도 알 수 있도록 컴파일 에러를 내는 식으로 작성하려면 어떻게 해야 하는지 고민
         toDoCard.title = todoCardUpdateRequest.title
@@ -64,8 +65,12 @@ class ToDoCardController(
 
     @DeleteMapping("/{toDoCardId}")
     fun deleteToDoCard(@PathVariable toDoCardId: Long): ResponseEntity<Unit> {
+        // 이 부분을 추가해도 select query는 한 번만 나간다.
+        // deleteByIdOrNull() 기본 메서드는 없기에, 없는 id로 delete 시도하는 경우 확실한 에러 메시지를 주기 위함
+        val toDoCard = toDoCardRepository.findByIdOrNull(toDoCardId) ?: throw ModelNotFoundException("ToDoCard", toDoCardId)
+
         return ResponseEntity
             .status(200)
-            .body(toDoCardRepository.deleteById(toDoCardId))
+            .body(toDoCardRepository.delete(toDoCard))
     }
 }
