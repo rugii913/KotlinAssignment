fba43f811eaad1533512c3d3d1e7235021a0deea
rugii913 <rugii913@gmail.com>
2023-12-28 16:52:36 +0900

    request dto에서 타입 잘못된 부분 수정
    - request dto에서 타입 잘못된 부분 수정
      - String → String?

--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardCreateRequest.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardCreateRequest.kt
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 data class ToDoCardCreateRequest(
     val title: String,
-    val description: String,
+    val description: String?,
     val userName: String,
     val createdDateTime: LocalDateTime,
 )
--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardUpdateRequest.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/dto/ToDoCardUpdateRequest.kt
@@ -1,8 +1,6 @@
 package kotlinassignment.week4.domain.toDoCard.dto
 
-import java.time.LocalDateTime
-
 data class ToDoCardUpdateRequest(
     val title: String,
-    val description: String,
+    val description: String?,
 )
