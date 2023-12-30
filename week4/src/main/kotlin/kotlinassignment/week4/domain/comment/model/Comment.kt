82bc3db2779991316ef0ba8061fd810e5b3f3b38
rugii913 <rugii913@gmail.com>
2023-12-30 15:49:20 +0900

    Comment entity 작성 및 ToDoCard와 연관관계 맺어줌

---
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/comment/model/Comment.kt
@@ -0,0 +1,30 @@
+package kotlinassignment.week4.domain.comment.model
+
+import jakarta.persistence.*
+import kotlinassignment.week4.domain.comment.dto.CommentResponse
+import kotlinassignment.week4.domain.toDoCard.model.ToDoCard
+import java.time.LocalDateTime
+
+@Entity
+class Comment(
+    @Column(nullable = false)
+    var content: String,
+
+    @Column(nullable = false)
+    val userName: String,
+
+    @Column(nullable = false)
+    val password: String,
+
+    @Column(nullable = false)
+    val createdDateTime: LocalDateTime,
+
+    @ManyToOne
+    @JoinColumn(name = "to_do_card_id", nullable = false)
+    val toDoCard: ToDoCard,
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+}
--- week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/model/ToDoCard.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/domain/toDoCard/model/ToDoCard.kt
@@ -1,7 +1,9 @@
 package kotlinassignment.week4.domain.toDoCard.model
 
 import jakarta.persistence.*
+import kotlinassignment.week4.domain.comment.model.Comment
 import kotlinassignment.week4.domain.toDoCard.dto.ToDoCardResponse
+import kotlinassignment.week4.domain.toDoCard.dto.ToDoCardResponseWithComments
 import java.time.LocalDateTime
 
 @Entity
@@ -31,5 +33,16 @@ fun ToDoCard.toResponse(): ToDoCardResponse {
         description = this.description,
         userName = this.userName,
         createdDateTime = this.createdDateTime,
+    )
+}
+
+fun ToDoCard.toResponseWithComments(): ToDoCardResponseWithComments {
+    return ToDoCardResponseWithComments(
+        id = this.id!!,
+        title = this.title,
+        description = this.description,
+        userName = this.userName,
+        createdDateTime = this.createdDateTime,
+        comments = this.comments.map { it.toResponse() }
     )
 }
