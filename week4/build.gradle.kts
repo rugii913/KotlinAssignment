348838675d546704ce3abe32835291aa73a910dd
rugii913 <rugii913@gmail.com>
2024-01-04 15:06:25 +0900

    Spring Boot 버전, 플러그인 버전 업
    - oauth client 관련 작업 중 RestClient 사용 위해 Spring Boot 버전 업

--- week4/build.gradle.kts
+++ week4/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.6"
+    id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
-    kotlin("plugin.noarg") version "1.8.22"
-    kotlin("kapt") version "1.8.22"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.noarg") version "1.9.21"
+    kotlin("kapt") version "1.9.21"
 }
 
 group = "kotlinassignment"
