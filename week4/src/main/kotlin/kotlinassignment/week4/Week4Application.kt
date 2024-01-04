b799e475c73e92e0fbea05efb6de6c11e20cb16b
rugii913 <rugii913@gmail.com>
2024-01-04 11:15:10 +0900

    더 알아볼 것, 해결 과제들 주석 정리

--- week4/src/main/kotlin/kotlinassignment/week4/Week4Application.kt
+++ week4/src/main/kotlin/kotlinassignment/week4/Week4Application.kt
@@ -12,13 +12,14 @@ fun main(args: Array<String>) {
 /**
  * TODO
  *  - ToDoCard.kt
- *    - 50줄 ToDoCard의 comments를 사용하지 않고, comment repository에서 페이징 처리한 comment를 가져오며 사용하지 않게 된 메서드
+ *    - 55줄 ToDoCard의 comments를 사용하지 않고, comment repository에서 페이징 처리한 comment를 가져오며 사용하지 않게 된 메서드
  *      ToDoCard가 참조하고 있는 comments를 사용하지 않고, comment repository 쪽에서 따로 Page 처리된 Comment들을 가져옴
  *      ToDoCard에서 직접 comments를 꺼내올 때도 paging 처리되게 하려면 어떻게 해야하는지 알아보기
  *  - ToDoCardService.kt
- *    - 32줄 ToDoCard가 참조하고 있는 comments를 사용하지 않고, comment repository 쪽에서 따로 Page 처리된 Comment들을 가져옴
+ *    - 31줄 ToDoCard가 참조하고 있는 comments를 사용하지 않고, comment repository 쪽에서 따로 Page 처리된 Comment들을 가져옴
  *          ToDoCard에서 직접 comments를 꺼내올 때도 paging 처리되게 하려면 어떻게 해야하는지 알아보기
- *    - 35줄 이 부분은 임시로 값을 넣어놨음, 추후 제대로 처리할 것
+ *    - 34줄 이 부분은 임시로 값을 넣어놨음, 추후 제대로 처리할 것
+ *    - 43줄 길이 검증 - 더 나은 방법이 없는지 고민
  *  - ToDoCardIsCompletePatchRequest.kt
  *    - 20줄 작동 방식 알아보기 - 생각보다 작동 방식이 어려운 듯함
  *      private이 빠지면 Jackson 매핑 에러가 발생하고, val isComplete = _isComplete!!으로 받아오려고 하면 validation이 안 됨
