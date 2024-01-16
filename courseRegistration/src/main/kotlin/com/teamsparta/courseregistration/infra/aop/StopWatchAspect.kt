abd1708b764320bb6af4ba648c33e6e7a4332c2a
rugii913 <rugii913@gmail.com>
2024-01-16 14:52:30 +0900

    (예제 관련)2강 강의자료 마지막 부분, DTO에 내용 채워서 보내기
    - 강의자료 마지막 부분, 빈 DTO 응답하는 오류가 있는 코드라 함
    - 이 코드를 어떻게 바꾸면 정확한 응답을 내보낼 수 있는지?
      - 일단 이런 식으로 작성하긴 했는데, 맞을지 모르겠다.

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/aop/StopWatchAspect.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/aop/StopWatchAspect.kt
@@ -14,11 +14,11 @@ class StopWatchAspect {
     private val logger = LoggerFactory.getLogger("Execution Time Logger")
 
     @Around("@annotation(com.teamsparta.courseregistration.infra.aop.StopWatch)")
-    fun run(joinPoint: ProceedingJoinPoint) {
+    fun run(joinPoint: ProceedingJoinPoint): Any {
         val stopWatch = StopWatch()
 
         stopWatch.start()
-        joinPoint.proceed()
+        val result = joinPoint.proceed()
         stopWatch.stop()
 
         val methodName = joinPoint.signature.name
@@ -26,5 +26,7 @@ class StopWatchAspect {
 
         val timeElapsedMs = stopWatch.totalTimeMillis
         logger.info("Method Name: $methodName | Arguments: ${methodArguments.joinToString(", ")} | Execution Time: ${timeElapsedMs}ms")
+
+        return result
     }
 }
