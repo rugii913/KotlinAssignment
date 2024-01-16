617f0625ead6a7f049ac38505ddbff86b260bd31
rugii913 <rugii913@gmail.com>
2024-01-16 18:13:46 +0900

    (예제)기획상 지정할 수 있는 값을 companion object에 넣지 않고, application.yml 활용
    - application.yml에 몇 가지 프로퍼티들을 가져와서 쓰게 함
      - 기획상 지정할 수 있는 값이 바뀔 수도 있다.
      - 이를 companion object에 넣어 두면 코드를 찾아 변경하고 다시 빌드, 배포해야하는 귀찮음이 생긴다.
      - application.yml에 입력해두고 이를 활용하게 하면 조금 더 편해진다.
      - @ Value 어노테이션 사용해서 값을 받아옴
        - (cf.) 아예 DTO를 하나 만들고 @ ConfigurationProperties를 이용해서 주입받게 할 수도 있다. <https://www.baeldung.com/kotlin/spring-boot-configurationproperties>

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/jwt/JwtPlugin.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/jwt/JwtPlugin.kt
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 import java.time.Duration
@@ -11,24 +12,22 @@ import java.time.Instant
 import java.util.*
 
 @Component
-class JwtPlugin { // JwtProvider, JwtUtil 등의 네이밍도 가능
-
-    companion object {
-        const val SECRET = "PO4c8z41Hia5gJG3oeuFJMRYBB4Ws4aZ" // 의미 없는 32자 random string
-        const val ISSUER = "team.sparta.com"
-        const val ACCESS_TOKEN_EXPIRATION_HOUR: Long = 168
-    }
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long, // application.yml에 있는 property를 @Value로 가져옴
+) { // JwtProvider, JwtUtil 등의 네이밍도 가능
 
     fun validateToken(jwt: String): Result<Jws<Claims>> { // try catch 사용하는 대신 Result을 사용(Kotlin에서 예외를 처리하는 한 방법 - 메서드 호출한 쪽에서 exception handling 할 수 있게 함)
         return kotlin.runCatching {
-            val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
+            val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
             Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt) // 위 key로 서명을 검증하고, 만료시간을 체크
         }
         // 더 응용해서 사용하고 싶은 경우 공식 문서 및 예시를 더 찾아볼 것
     }
 
     fun generateAccessToken(subject: String, email: String, role: String): String {
-        return generateToken(subject, email, role, Duration.ofHours(ACCESS_TOKEN_EXPIRATION_HOUR))
+        return generateToken(subject, email, role, Duration.ofHours(accessTokenExpirationHour))
     }
 
     // refresh token 메서드 만들 때를 생각해서 메서드 분리
@@ -38,13 +37,13 @@ class JwtPlugin { // JwtProvider, JwtUtil 등의 네이밍도 가능
             .add(mapOf("role" to role, "email" to email))
             .build()
 
-        val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
+        val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
         val now = Instant.now()
 
         // registered claim은 이미 builder에 작성이 되어 있다.
         return Jwts.builder()
             .subject(subject)
-            .issuer(ISSUER)
+            .issuer(issuer)
             .issuedAt(Date.from(now))
             .expiration(Date.from(now.plus(expirationPeriod)))
             .claims(claims)
