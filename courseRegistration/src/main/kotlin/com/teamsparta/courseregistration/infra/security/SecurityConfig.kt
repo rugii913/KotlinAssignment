235e50aac58ff2fac8e283d8e883147cbeb8efea
rugii913 <rugii913@gmail.com>
2024-01-17 11:57:49 +0900

    (예제)작성한 필터를 필터 체인에 적용
    작성한 필터를 필터 체인에 적용
    - requestMatchers(~)에 명시한 URI는 모든 접근을 허용하고,
      - 그 외의 URI는 모두 인증을 거치게 함
      - 자세히 말하면 명시한 URI에서는 Authentication 객체가 없거나, 해당 객체의 isAuthenticated()가 false여도 Spring Security에서 예외처리를 하지 않음
    
    - 나머지 URI에 대해서는 추가한 JwtAuthenticationFilter에서 인증을 판단

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
@@ -1,15 +1,18 @@
 package com.teamsparta.courseregistration.infra.security
 
+import com.teamsparta.courseregistration.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity // Http 기반 통신 시 보안 기능 관련
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -17,7 +20,19 @@ class SecurityConfig {
             .httpBasic { it.disable() } // BasicAuthenticationFilter, DefaultLoginPageGeneratingFilter, DefaultLogoutPageGeneratingFilter 제외
             .formLogin { it.disable() } // UsernamePasswordAuthenticationFilter, DefaultLoginPageGeneratingFilter, DefaultLogoutPageGeneratingFilter 제외
             .csrf { it.disable() } // CsrfFilter 제외
-            .build() // 기본 필터들에서 위 필터들을 꺼준 것
+            // 기본 필터들에서 위 필터들을 꺼준 것
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/login",
+                    "/signup",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                ).permitAll() // 위에 명시한 URI를 제외하고는 모두 인증을 거치게 함
+                    .anyRequest().authenticated()
+            }
+            // 기존 UsernamePasswordAuthenticationFilter가 존재하던 자리에 JwtAuthenticationFilter 적용
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
         /*
         그런데 이 부분 문법이 이해가 안 된다...
         val function: (t: HttpBasicConfigurer<HttpSecurity>) -> Unit = { it.disable() }
