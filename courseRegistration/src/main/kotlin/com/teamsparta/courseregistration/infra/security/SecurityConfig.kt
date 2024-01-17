c9be19253bc7d169fa59088482df6a9d0a02f22b
rugii913 <rugii913@gmail.com>
2024-01-17 11:59:10 +0900

    (예제 관련)a78c5ad에서 문법 이해되지 않았던 부분 설명 주석 추가
    - a78c5ad에서 문법 이해되지 않았던 부분 설명 주석 추가
      - 람다, 함수형 인터페이스 관련

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/infra/security/SecurityConfig.kt
@@ -34,9 +34,19 @@ class SecurityConfig(
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
         /*
+        (???)
         그런데 이 부분 문법이 이해가 안 된다...
+        httpBasic { it.disable() }
+        
+        풀어서 쓰면
         val function: (t: HttpBasicConfigurer<HttpSecurity>) -> Unit = { it.disable() }
         http.httpBasic(function)
+        
+        ==> 여기서 httpBasic의 매개변수인 org.springframework.security.config.Customizer는 함수형 인터페이스
+        ====> 이 함수형 인터페이스가 갖고 있는 void customize(T t)를 바로 람다로 구현해준 것 - 람다이므로 it을 사용한 것임
+        ==> 또한 Customizer의 제네릭 타입이 Customizer<HttpBasicConfigurer<HttpSecurity>>이므로
+        ====> it은 T인 HttpBasicConfigurer 타입 객체를 가리키게 됨 
+        ====> T인 HttpBasicConfigurer의 (정확하게는 그 상위 타입인 AbstractHttpConfigurer에서 구현된) disable()을 호출한 것
          */
     }
 }
