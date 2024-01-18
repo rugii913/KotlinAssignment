b135ee41b21f6c67eb2965120b539ab34ee2bf5c
rugii913 <rugii913@gmail.com>
2024-01-18 17:51:52 +0900

    테이블 스키마 변경, to_do_card에서 user_name이 아닌 member_id를 갖고 있게 함
    - 테이블 스키마 변경, to_do_card에서 user_name이 아닌 member_id를 갖고 있게 함
    - 이에 따른 테스트 데이터 변경

--- week4/src/main/resources/data.sql
+++ week4/src/main/resources/data.sql
@@ -1,9 +1,10 @@
-insert into to_do_card(title, description, user_name, created_date_time)
-values ('제목1', '할 일 상세1', '유저1', CURRENT_TIMESTAMP),
-       ('두번째 글 제목', '상세한 할 일 2', '이름2', CURRENT_TIMESTAMP),
-       ('3제목', '일2 상세 내용', '세번째 user_name', CURRENT_TIMESTAMP);
+insert into to_do_card(title, description, member_id, created_date_time)
+values ('제목1', '할 일 상세1', 1, CURRENT_TIMESTAMP - 25),
+       ('두번째 글 제목', '상세한 할 일 2', 1, CURRENT_TIMESTAMP - 20),
+       ('3제목', '일2 상세 내용', 1, CURRENT_TIMESTAMP);
+
 insert into comment(content, user_name, password, created_date_time, to_do_card_id)
-values ('댓글이야', '이건 이름1', 'test!', CURRENT_TIMESTAMP, 1),
-       ('2댓글', 'user_name2', 'test!', CURRENT_TIMESTAMP, 1),
-       ('댓글 내용입니다', 'abcd3', 'test!', CURRENT_TIMESTAMP, 2),
-       ('아무 댓글', '4이름', 'test!', CURRENT_TIMESTAMP, 2);
+values ('댓글이야', '이건 이름1', 'test!', CURRENT_TIMESTAMP - 20, 1),
+       ('2댓글', 'user_name2', 'test!', CURRENT_TIMESTAMP - 15, 1),
+       ('댓글 내용입니다', 'abcd3', 'test!', CURRENT_TIMESTAMP - 10, 2),
+       ('아무 댓글', '4이름', 'test!', CURRENT_TIMESTAMP - 5, 2);
