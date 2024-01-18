04ab091e183d60033e89bc37714a708db5b43970
rugii913 <rugii913@gmail.com>
2024-01-19 01:27:56 +0900

    수동 테스트를 위해 Member 및 Comment 테이블 스키마 수정 및 테스트 데이터 수정
    - 수동 테스트를 위해 Member 및 Comment 테이블 스키마 수정 및 테스트 데이터 수정
      - 현재 큰 문제 없이 작동하는 것으로 보임

--- week4/src/main/resources/data.sql
+++ week4/src/main/resources/data.sql
@@ -5,8 +5,8 @@ values ('제목1', '할 일 상세1', 1, CURRENT_TIMESTAMP - 25),
        ('4제목', '4내용', 1, CURRENT_TIMESTAMP - 10),
        ('제목5', '내용5', 2, CURRENT_TIMESTAMP- 5);
 
-insert into comment(content, user_name, password, created_date_time, to_do_card_id)
-values ('댓글이야', '이건 이름1', 'test!', CURRENT_TIMESTAMP - 20, 1),
-       ('2댓글', 'user_name2', 'test!', CURRENT_TIMESTAMP - 15, 1),
-       ('댓글 내용입니다', 'abcd3', 'test!', CURRENT_TIMESTAMP - 10, 2),
-       ('아무 댓글', '4이름', 'test!', CURRENT_TIMESTAMP - 5, 2);
+insert into comment(content, member_id, created_date_time, to_do_card_id)
+values ('댓글이야', '1', CURRENT_TIMESTAMP - 20, 1),
+       ('2댓글', '1', CURRENT_TIMESTAMP - 15, 1),
+       ('댓글 내용입니다', '1', CURRENT_TIMESTAMP - 10, 2),
+       ('아무 댓글', '2', CURRENT_TIMESTAMP - 5, 2);
