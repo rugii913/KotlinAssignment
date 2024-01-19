29a829f2693bc494c873802990d4abce89ed5710
rugii913 <rugii913@gmail.com>
2024-01-19 11:12:31 +0900

    GlobalExceptionHandler에서 인증, 인가 예외도 처리하도록 함
    GlobalExceptionHandler에서 인증, 인가 예외도 처리하도록 함

--- week4and8/src/main/kotlin/kotlinassignment/week4and8/domain/member/exception/InvalidCredentialException.kt
+++ week4and8/src/main/kotlin/kotlinassignment/week4and8/domain/member/exception/InvalidCredentialException.kt
@@ -1,5 +1,5 @@
 package kotlinassignment.week4and8.domain.member.exception
 
 class InvalidCredentialException(
-    override val message: String? = "The credential is invalid"
+    override val message: String? = "자격 증명이 필요합니다."
 ): RuntimeException()
