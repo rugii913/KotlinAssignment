83d5b5e09dcf851ca937ff773d9e23b547ad7c44
rugii913 <rugii913@gmail.com>
2024-02-01 18:18:04 +0900

    (예제 관련)CourseStatus 바로 enum 인자로 받아오기

--- courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/model/CourseStatus.kt
+++ courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/model/CourseStatus.kt
@@ -1,6 +1,16 @@
 package com.teamsparta.courseregistration.domain.course.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import org.apache.commons.lang3.EnumUtils
+
 enum class CourseStatus {
     OPEN,
-    CLOSED
+    CLOSED;
+
+    companion object {
+        @JvmStatic
+        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+        fun parse(name: String?): CourseStatus? =
+            name?.let { EnumUtils.getEnumIgnoreCase(CourseStatus::class.java, it.trim()) }
+    }
 }
