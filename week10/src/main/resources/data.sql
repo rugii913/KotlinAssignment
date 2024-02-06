b9ef60820897e53acdc34710c5c6702cc781b1ad
rugii913 <rugii913@gmail.com>
2024-02-06 17:16:54 +0900

    과제 기반 수정-스키마 중 컬럼 추가, 컬럼 순서 변경, 로컬 테스트 데이터 반영
    - 스키마 중 컬럼 추가 및 이름 변경
      - created_date_time → created_at으로 변경
      - last_modified_at 컬럼 추가
    - 컬럼 순서 변경
      - 메타 데이터들이 더 앞쪽에 오도록 컬럼 순서 변경
    - 로컬 테스트 데이터 반영

--- week10/src/main/resources/data.sql
+++ week10/src/main/resources/data.sql
@@ -1,19 +1,19 @@
-insert into to_do_card(title, description, member_id, created_date_time)
-values ('제목1', '할 일 상세1', 1, CURRENT_TIMESTAMP - 25),
-       ('두번째 글 제목', '상세한 할 일 2', 1, CURRENT_TIMESTAMP - 20),
-       ('3제목', '일3 상세 내용', 2, CURRENT_TIMESTAMP - 15),
-       ('4제목', '4내용', 1, CURRENT_TIMESTAMP - 10),
-       ('제목5', '내용5', 2, CURRENT_TIMESTAMP- 5);
+insert into to_do_card(created_at, last_modified_at, title, description, member_id)
+values (CURRENT_TIMESTAMP - 25, CURRENT_TIMESTAMP - 25, '제목1', '할 일 상세1', 1),
+       (CURRENT_TIMESTAMP - 20, CURRENT_TIMESTAMP - 20, '두번째 글 제목', '상세한 할 일 2', 1),
+       (CURRENT_TIMESTAMP - 15, CURRENT_TIMESTAMP - 15, '3제목', '일3 상세 내용', 2),
+       (CURRENT_TIMESTAMP - 10, CURRENT_TIMESTAMP - 10, '4제목', '4내용', 1),
+       (CURRENT_TIMESTAMP - 5, CURRENT_TIMESTAMP - 5, '제목5', '내용5', 2);
 
-insert into comment(content, member_id, created_date_time, to_do_card_id)
-values ('댓글이야', 1, CURRENT_TIMESTAMP - 20, 1),
-       ('2댓글', 2, CURRENT_TIMESTAMP - 15, 1),
-       ('댓글 내용입니다', 1, CURRENT_TIMESTAMP - 10, 2),
-       ('아무 댓글', 2, CURRENT_TIMESTAMP - 5, 2);
+insert into comment(created_at, last_modified_at, content, member_id, to_do_card_id)
+values (CURRENT_TIMESTAMP - 20, CURRENT_TIMESTAMP - 20, '댓글이야', 1, 1),
+       (CURRENT_TIMESTAMP - 15, CURRENT_TIMESTAMP - 15, '2댓글', 2, 1),
+       (CURRENT_TIMESTAMP - 10, CURRENT_TIMESTAMP - 10, '댓글 내용입니다', 1, 2),
+       (CURRENT_TIMESTAMP - 5, CURRENT_TIMESTAMP - 5, '아무 댓글', 2, 2);
 
 -- member 로컬 테스트 데이터
 -- 1. email: test, password: test
 -- 2. email: test2, password: test
-insert into member(email, password, nickname)
-values ('test', '$2a$10$4SGkZKr9t8xKCVaxMc/o7ugMbfcxZA00AszP7Z/mFVOkdZD0xZNaK', 'test'),
-       ('test2', '$2a$10$4SGkZKr9t8xKCVaxMc/o7ugMbfcxZA00AszP7Z/mFVOkdZD0xZNaK', 'test2');
+insert into member(created_at, last_modified_at, email, password, nickname)
+values (CURRENT_TIMESTAMP - 20, CURRENT_TIMESTAMP - 20, 'test', '$2a$10$4SGkZKr9t8xKCVaxMc/o7ugMbfcxZA00AszP7Z/mFVOkdZD0xZNaK', 'test'),
+       (CURRENT_TIMESTAMP - 15, CURRENT_TIMESTAMP - 15, 'test2', '$2a$10$4SGkZKr9t8xKCVaxMc/o7ugMbfcxZA00AszP7Z/mFVOkdZD0xZNaK', 'test2');
